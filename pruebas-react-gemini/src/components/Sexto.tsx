9800e0b09246c35230b73f15a7cc4a1109b560b6
Lucas Caballero <caballerolucas18@gmail.com>
2025-10-30 16:53:19 -0300

    finalizado sexto

--- pruebas-react-gemini/src/components/Sexto.tsx
+++ pruebas-react-gemini/src/components/Sexto.tsx
@@ -1,13 +1,34 @@
+import { useEffect, useState } from "react"
 
 
 export const Sexto = () => {
 
-    const 
+    const [temporizador, setTemporizador] = useState(0)
+    const [parar, setParar] = useState(true)
+    useEffect(()=>{
+      setInterval(() => {
+        setTemporizador(temporizador + 1)
+      }, 1000)
+
+
+
+    })
+
+    const handlePausa = () =>{
+      setTemporizador( 0);
+      clearTimeout(temporizador);
+      clearInterval(temporizador);
+      setParar(false)
+    }
 
 
   return (
     <div>
+      {
+        parar ? 'Soy un temporizador: '+ temporizador : 'No soy un temporizador'
+      }
 
+      <button onClick={handlePausa}>Pausa</button>
     </div>
   )
 }
